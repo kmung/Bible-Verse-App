d1096261d40b153e66bf31e7e3738829ac1574cf
Khawm Mung <johnmungtawng5@gmail.com>
2023-02-12 01:38:45 -0600

    initial commit

---
+++ app/src/main/java/edu/oru/cit352/ckmung1/bibleverseapp/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.oru.cit352.ckmung1.bibleverseapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
