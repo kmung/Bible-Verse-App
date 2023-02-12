4aec39d43ad167c9bab40b9314eee3b250ea913a
Khawm Mung <johnmungtawng5@gmail.com>
2023-02-12 02:17:09 -0600

    update ui colors

--- app/src/main/java/edu/oru/cit352/ckmung1/bibleverseapp/MainActivity.java
+++ app/src/main/java/edu/oru/cit352/ckmung1/bibleverseapp/MainActivity.java
@@ -2,6 +2,7 @@ package edu.oru.cit352.ckmung1.bibleverseapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    // Bible display function
+    private void displayVerse() {
+        // getting the resource
+        Resources res = getResources();
     }
 }
